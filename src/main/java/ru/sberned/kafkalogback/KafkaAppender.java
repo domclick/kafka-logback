917067972a6c067c8d3d612b1a7b22bd6a023084
asm0dey <finkelshtein@domclick.ru>
2017-05-03 23:35:54 +0300

    Prepaartion for maven central

--- src/main/java/ru/sberned/kafkalogback/KafkaAppender.java
+++ src/main/java/ru/sberned/kafkalogback/KafkaAppender.java
@@ -15,23 +15,23 @@ import java.util.Properties;
 
 /**
  * This appender expects topic, bootstrapServers, valueSerializer and layout as mandatory.
- * You could also supply additional kafka customProps in <customProps> tag in the following manner
- * <customProps>key1=value1,key2=value2</customProps>
- * <p>
+ * You could also supply additional kafka customProps in &lt;customProps&gt; tag in the following manner
+ * &lt;customProps&gt;key1=value1,key2=value2&lt;/customProps&gt;
+ * &lt;p&gt;
  * Sample configuration
- * <?xml version="1.0" encoding="UTF-8" ?>
- * <configuration>
- *     <appender name="KAFKA" class="ru.sberned.kafkalogback.KafkaAppender">
- *         <topic>test-topic</topic>
- *         <bootstrapServers>localhost:2181</bootstrapServers>
- *         <layout class="ru.sberned.kafkalogback.CustomJsonLayout">
- *             <jsonFormatter class="ch.qos.logback.contrib.jackson.JacksonJsonFormatter"/>
- *         </layout>
- *     </appender>
- *     <root level="trace">
- *         <appender-ref ref="KAFKA" />
- *     </root>
- * </configuration>
+ * &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; ?&gt;
+ * &lt;configuration&gt;
+ *     &lt;appender name=&quot;KAFKA&quot; class=&quot;ru.sberned.kafkalogback.KafkaAppender&quot;&gt;
+ *         &lt;topic&gt;test-topic&lt;/topic&gt;
+ *         &lt;bootstrapServers&gt;localhost:2181&lt;/bootstrapServers&gt;
+ *         &lt;layout class=&quot;ru.sberned.kafkalogback.CustomJsonLayout&quot;&gt;
+ *             &lt;jsonFormatter class=&quot;ch.qos.logback.contrib.jackson.JacksonJsonFormatter&quot;/&gt;
+ *         &lt;/layout&gt;
+ *     &lt;/appender&gt;
+ *     &lt;root level=&quot;trace&quot;&gt;
+ *         &lt;appender-ref ref=&quot;KAFKA&quot; /&gt;
+ *     &lt;/root&gt;
+ * &lt;/configuration&gt;
  */
 @Setter
 public class KafkaAppender extends AppenderBase<ILoggingEvent> {
